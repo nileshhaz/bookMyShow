cc6a984f24a4cec2f8bd42373d7956322d9dbe95
coldBlood <85475503+nileshhaz@users.noreply.github.com>
2022-04-13 19:30:37 +0530

    'bookMyShow'

--- src/Components/Sliders/BelowHeader.components.js
+++ src/Components/Sliders/BelowHeader.components.js
@@ -3,68 +3,133 @@ import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import axios from "axios";
 import Image from "react-bootstrap/Image";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function BelowHeader() {
-    const [movie, setMovie] = useState([]);
-    useEffect(() => {
-        async function getMovies() {
-            try {
-                const response = await axios.get('http://localhost:5000/bookmyshow/movies/getMovies');
-                console.log(response);
-                setMovie(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getMovies();
-    }, []);
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const options = {
+          method: "GET",
+          url: "https://movie-database-alternative.p.rapidapi.com/",
+          params: { s: "avengers", r: "json", page: "1" },
+          headers: {
+            "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+            "X-RapidAPI-Key":
+              "7595d28e6fmsh2af7c4fbb9d33a1p17adecjsn032c75c91f3f",
+          },
+        };
 
-    return (
-        <div style={{ backgroundColor:"rgb(245, 245, 245)",padding: "2% 9% 2% 9%" }}>
-            <h2 style={{fontSize:"1.5rem", fontWeight:"bold"}}>Recommended Movies</h2>
-            <Carousel indicators={false} variant="Light">
-                <Carousel.Item >
-                    <CardGroup style={{display:"flex", gap:"40px", justifyContent:"space-around", outline:"none"}}>
-                        {movie.map((mov, index) => {
-                            if (index < 5)
-                                return (
-                                    <Card border="light" style={{backgroundColor:"rgb(245, 245, 245)",height:"450px", width:"200px"}}>
-                                        <Card.Img variant="top" src={mov.imageUrl} style={{borderRadius:"5%",height:"80%", width:"100%"}}/>
-                                        <Card.Body>
-                                            <Card.Title>{mov.movieName}</Card.Title>
-                                            <Card.Text>
-                                                {mov.movieGenre}
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>)
-                        })}
-                    </CardGroup>
-                </Carousel.Item>
-                <Carousel.Item >
-                    <CardGroup style={{display:"flex", gap:"40px", justifyContent:"space-around"}}>
-                    {movie.map((mov, index) => {
-                            if (index >= 5)
-                                return (
-                                    <Card border="light" style={{backgroundColor:"rgb(245, 245, 245)",height:"450px", width:"200px"}}>
-                                        <Card.Img variant="top" src={mov.imageUrl} style={{borderRadius:"5%",height:"80%", width:"100%"}}/>
-                                        <Card.Body>
-                                            <Card.Title>{mov.movieName}</Card.Title>
-                                            <Card.Text>
-                                                {mov.movieGenre}
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>)
-                        })}
-                    </CardGroup>
-                </Carousel.Item>
-            </Carousel>
+        axios
+          .request(options)
+          .then(function (response) {
+            console.log(".................", response.data);
+            setMovie(response.data.Search);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+        // const response = await axios.get(
+        //   "https://movie-database-alternative.p.rapidapi.com/"
+        // );
+        // console.log(response);
+        // setMovie(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getMovies();
+  }, []);
 
-            <Image style={{margin:"7.5% 0 4.5% 0"}}src="./banner2.png" fluid />
+  return (
+    <div
+      style={{ backgroundColor: "rgb(245, 245, 245)", padding: "2% 9% 2% 9%" }}
+    >
+      <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+        Recommended Movies
+      </h2>
+      <Carousel indicators={false} variant="Light">
+        <Carousel.Item>
+          <CardGroup
+            style={{
+              display: "flex",
+              gap: "40px",
+              justifyContent: "space-around",
+              outline: "none",
+            }}
+          >
+            {movie.map((mov, index) => {
+              if (index < 5)
+                return (
+                  <Card
+                    border="light"
+                    style={{
+                      backgroundColor: "rgb(245, 245, 245)",
+                      height: "450px",
+                      width: "200px",
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={mov.Poster}
+                      style={{
+                        borderRadius: "5%",
+                        height: "80%",
+                        width: "100%",
+                      }}
+                    />
+                    <Card.Body>
+                      <Card.Title>{mov.Title}</Card.Title>
+                      <Card.Text>{`${mov.Type} , ${mov.Year}`}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                );
+            })}
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CardGroup
+            style={{
+              display: "flex",
+              gap: "40px",
+              justifyContent: "space-around",
+            }}
+          >
+            {movie.map((mov, index) => {
+              if (index >= 5)
+                return (
+                  <Card
+                    border="light"
+                    style={{
+                      backgroundColor: "rgb(245, 245, 245)",
+                      height: "450px",
+                      width: "200px",
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={mov.Poster}
+                      style={{
+                        borderRadius: "5%",
+                        height: "80%",
+                        width: "100%",
+                      }}
+                    />
+                    <Card.Body>
+                      <Card.Title>{mov.Title}</Card.Title>
+                      <Card.Text>{`${mov.Type} , ${mov.Year}`}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                );
+            })}
+          </CardGroup>
+        </Carousel.Item>
+      </Carousel>
 
-
-        </div>
-    );
+      <Image style={{ margin: "7.5% 0 4.5% 0" }} src="./banner2.png" fluid />
+    </div>
+  );
 }
 
 export default BelowHeader;
--- src/Components/Sliders/entertainment.components.js
+++ src/Components/Sliders/entertainment.components.js
@@ -2,53 +2,123 @@ import Carousel from "react-bootstrap/Carousel";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import axios from "axios";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Entertainment() {
-    const [events, setevents] = useState([]);
-    useEffect(() => {
-        async function getMovies() {
-            try {
-                const response = await axios.get('http://localhost:5000/bookmyshow/ET/get');
-                console.log(response);
-                setevents(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getMovies();
-    }, []);
+  const [events, setevents] = useState([]);
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const options = {
+          method: "GET",
+          url: "https://movie-database-alternative.p.rapidapi.com/",
+          params: { s: "x men", r: "json", page: "1" },
+          headers: {
+            "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+            "X-RapidAPI-Key":
+              "7595d28e6fmsh2af7c4fbb9d33a1p17adecjsn032c75c91f3f",
+          },
+        };
 
-    return (
-        <div style={{ backgroundColor:"rgb(245, 245, 245)",padding: "0 9% 3.5% 9%" }}>
+        axios
+          .request(options)
+          .then(function (response) {
+            console.log(".................", response.data);
+            setevents(response.data.Search);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+        // const response = await axios.get(
+        //   "https://movie-database-alternative.p.rapidapi.com/"
+        // );
+        // console.log(response);
+        // setMovie(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getMovies();
+  }, []);
 
-            <h2 style={{fontSize:"1.7rem", fontWeight:"bold"}}>The Best Of Entertainment</h2>
-            <Carousel indicators={false}>
-                <Carousel.Item >
-                    <CardGroup style={{display:"flex", gap:"40px", justifyContent:"space-around", outline:"none"}}>
-                        {events.map((event, index) => {
-                            if (index < 5)
-                                return (
-                                    <Card border="light" style={{backgroundColor:"rgb(245, 245, 245)",height:"225px", width:"200px"}}>
-                                        <Card.Img variant="top" src={event.imageUrl} style={{borderRadius:"5%",height:"100%", width:"100%"}}/>
-                                    </Card>)
-                        })}
-                    </CardGroup>
-                </Carousel.Item>
-                <Carousel.Item >
-                    <CardGroup style={{display:"flex", gap:"40px", justifyContent:"space-around"}}>
-                    {events.map((event, index) => {
-                            if (index >= 5)
-                                return (
-                                    <Card border="light" style={{backgroundColor:"rgb(245, 245, 245)",height:"225px", width:"200px"}}>
-                                        <Card.Img variant="top" src={event.imageUrl} style={{borderRadius:"5%",height:"100%", width:"100%"}}/>
-                                    </Card>)
-                        })}
-                    </CardGroup>
-                </Carousel.Item>
-            </Carousel>
-        </div>
-    );
+  return (
+    <div
+      style={{ backgroundColor: "rgb(245, 245, 245)", padding: "0 9% 3.5% 9%" }}
+    >
+      <h2 style={{ fontSize: "1.7rem", fontWeight: "bold" }}>
+        The Best Of Entertainment
+      </h2>
+      <Carousel indicators={false}>
+        <Carousel.Item>
+          <CardGroup
+            style={{
+              display: "flex",
+              gap: "40px",
+              justifyContent: "space-around",
+              outline: "none",
+            }}
+          >
+            {events.map((event, index) => {
+              if (index < 5)
+                return (
+                  <Card
+                    border="light"
+                    style={{
+                      backgroundColor: "rgb(245, 245, 245)",
+                      height: "225px",
+                      width: "200px",
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={event.Poster}
+                      style={{
+                        borderRadius: "5%",
+                        height: "100%",
+                        width: "100%",
+                      }}
+                    />
+                  </Card>
+                );
+            })}
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CardGroup
+            style={{
+              display: "flex",
+              gap: "40px",
+              justifyContent: "space-around",
+            }}
+          >
+            {events.map((event, index) => {
+              if (index >= 5)
+                return (
+                  <Card
+                    border="light"
+                    style={{
+                      backgroundColor: "rgb(245, 245, 245)",
+                      height: "225px",
+                      width: "200px",
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={event.Poster}
+                      style={{
+                        borderRadius: "5%",
+                        height: "100%",
+                        width: "100%",
+                      }}
+                    />
+                  </Card>
+                );
+            })}
+          </CardGroup>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
 
 export default Entertainment;
